6c87e8c664397a9b25db8711340f2a6b0fd792dc
Vishruth Patil <vishrutppatil@gmail.com>
2017-06-08 23:05:21 +0530

    Add files via upload

---
+++ Theme2/assets/js/Theme2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    debugger;
+    var obj = localStorage.getItem("obj");
+    obj = JSON.parse(obj);
+    $('#FullName').append(obj.FullName);
+    $('#FullNam').append(obj.FullName);
+    $('#DOB').append(obj.DateOfBirth);
+    $('#Gender').append(obj.Gender);
+    $('#Country').append(obj.Country);
+    $('#About').append(obj.About);
+});
